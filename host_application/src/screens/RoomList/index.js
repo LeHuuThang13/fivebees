2ca28e705a501ff8dab9327ec6527ccec939c2ae
nguyenlam <nguyen60c@gmail.com>
2022-07-02 20:16:43 +0700

    features creating listing rooms

--- host_application/src/screens/RoomList/index.js
+++ host_application/src/screens/RoomList/index.js
@@ -1,12 +1,16 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {Modal, StyleSheet, Text, View} from 'react-native';
+import {Modal, StyleSheet, Text, Touchable, View} from 'react-native';
 import Container from '../../components/common/Container';
 import IconMenu from '../../assets/icons/menu_icon.svg';
 import {TouchableOpacity} from 'react-native';
 import colors from '../../assets/themes/colors';
 import ArrowDown from '../../assets/icons/arrowDown.svg';
 import {BuildingOptions} from '../../components/common/BuildingOptions/BuildingOptions';
+import Device from '../../assets/icons/device.svg';
+import Setting from '../../assets/icons/setting_white.svg';
+import BrokenDevice from '../../assets/icons/broken.svg';
+import styles from './styles';
 
 const RoomList = () => {
   const {setOptions, toggleDrawer} = useNavigation();
@@ -42,8 +46,49 @@ const RoomList = () => {
     setChooseBuilding(option);
   };
 
+  const Room = () => {
+    return (
+      <View style={styles.roomContainer}>
+        <View style={styles.headerRoom}>
+          <Text style={styles.headerRoomLeft}>Phòng 101</Text>
+
+          <Text style={styles.headerRoomRight}>
+            <View>
+              <Text style={styles.labelStatus}>Tình trạng:</Text>
+            </View>
+            <View style={styles.statusBlock}>
+              <Text style={styles.status}>Đang sử dụng</Text>
+            </View>
+          </Text>
+        </View>
+
+        <View style={styles.bodyRoom}>
+          <View style={styles.totalItem}>
+            <Text style={styles.iconItem}>
+              <Device />
+            </Text>
+            <Text style={styles.textTotalDevices}>Tổng thiết bị: 10</Text>
+          </View>
+          <View style={styles.totalItem}>
+            <Text style={styles.iconItem}>
+              <BrokenDevice />
+            </Text>
+            <Text style={styles.textTotalDevices}>Thiết bị hư hỏng: 0</Text>
+          </View>
+          <View style={styles.btnRoomContainer}>
+            <TouchableOpacity style={styles.btnRoom}>
+              <Setting />
+              <Text style={styles.textBtn}>Quản lý thiết bị</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <Container>
+      {/* Selecting options */}
       <View>
         <View style={[styles.selectOptionSection, styles.stylesText]}>
           <TouchableOpacity
@@ -66,32 +111,16 @@ const RoomList = () => {
           />
         </Modal>
       </View>
+
+      {/* rooms */}
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
     </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  selectOptionSection: {
-    borderBottomWidth: 2,
-    borderBottomColor: colors.secondary,
-    maxWidth: 100,
-    marginTop: 16,
-  },
-
-  textSelectColor: {
-    color: colors.secondary,
-    flex: 1,
-  },
-
-  stylesText: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-
-  opacityBtn: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-});
 
 export default RoomList;
