842fa5d23bdcbf908611c4ed858ae42fb96ab790
nguyenlam <nguyen60c@gmail.com>
2022-07-06 11:22:00 +0700

    features menu popup more managing

--- host_application/src/screens/Managing/index.js
+++ host_application/src/screens/Managing/index.js
@@ -1,7 +1,6 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {Modal, Text, TouchableOpacity, View} from 'react-native';
-import colors from '../../assets/themes/colors';
+import {Text, TouchableOpacity, View} from 'react-native';
 import MenuIcon from '../../assets/icons/menu_icon.svg';
 import Container from '../../components/common/Container';
 import styles from './styles';
@@ -10,70 +9,33 @@ import Room from '../../assets/icons/room_outline.svg';
 import Dots from '../../assets/icons/dots.svg';
 import {BUILDINGDETAILS} from '../../constants/routeNames';
 import SettingHeaderNavigator from '../../utils/SettingHeaderNavigator';
+import MorePopupMenu from '../../components/common/MorePopupMenu';
 
 const Managing = ({navigation, route}) => {
-  const [elementVisible, setElementVisible] = useState(false);
   SettingHeaderNavigator.settingHeaderNavigator({MenuIcon: MenuIcon});
 
   const Building = () => {
     return (
       <View style={[styles.roomContainer]}>
-        <TouchableOpacity
-          onPress={() => setElementVisible(!elementVisible)}
-          style={{
-            position: 'absolute',
+        <MorePopupMenu
+          styles={{
             width: 30,
-            right: 10,
-            top: 12,
-            zIndex: 1,
             flexDirection: 'row-reverse',
-          }}>
-          <Text style={styles.headerRoomRight}>
-            <Dots />
-          </Text>
-        </TouchableOpacity>
-        {elementVisible ? (
-          <View style={{position: 'absolute', right: 30}}>
-            <View
-              style={[
-                styles.headerMore,
-                {
-                  width: 100,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: colors.border_input,
-                  backgroundColor: colors.white,
-                  zIndex: 1,
-                  top: 10,
-                  right: -20,
-                },
-              ]}>
-              <Modal
-                transparent={true}
-                animationType="none"
-                visible={elementVisible}
-                onRequestClose={() => setElementVisible(false)}>
-                <TouchableOpacity
-                  style={{}}
-                  onPress={() => {
-                    setElementVisible(false);
-                  }}>
-                  <View style={{backgroundColor: colors.white}}>
-                    <TouchableOpacity
-                      onPress={() => navigation.navigate(BUILDINGDETAILS)}
-                      style={{marginBottom: 20}}>
-                      <Text>Chỉnh sửa</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      onPress={() => setElementVisible(!elementVisible)}>
-                      <Text>Xóa</Text>
-                    </TouchableOpacity>
-                  </View>
-                </TouchableOpacity>
-              </Modal>
-            </View>
-          </View>
-        ) : null}
+            position: 'absolute',
+            right: 15,
+            top: 10,
+            zIndex: 1,
+          }}
+          actionNameEdit={'Chỉnh sửa'}
+          actionNameDelete={'Xóa'}
+          onPressEdit={() => {
+            console.log('Chỉnh sửa');
+          }}
+          onPressDelete={() => {
+            console.log('Xóa');
+          }}
+        />
+
         <View style={styles.headerRoom}>
           <Text style={styles.headerRoomLeft}>Tòa A</Text>
         </View>
@@ -101,17 +63,11 @@ const Managing = ({navigation, route}) => {
 
   return (
     <Container>
-      <TouchableOpacity
-        activeOpacity={1}
-        onPress={() => {
-          setElementVisible(false);
-        }}>
-        <Building />
-        <Building />
-        <Building />
-        <Building />
-        <Building />
-      </TouchableOpacity>
+      <Building />
+      <Building />
+      <Building />
+      <Building />
+      <Building />
     </Container>
   );
 };
