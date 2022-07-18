9a7eb86449fd946477fd4fe747b876592d6fae97
dangthiminhngoc356 <81677658+dangthiminhngoc356@users.noreply.github.com>
2022-07-18 11:44:05 +0700

    LoginUserScreen

---
+++ user_application/src/screens/HomeUserScreen/HomeUserScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeUserScreen = () => {
+  return (
+    <View>
+      <Text>HomeUserScreen</Text>
+    </View>
+  )
+}
+
+export default HomeUserScreen
+
+const styles = StyleSheet.create({})
