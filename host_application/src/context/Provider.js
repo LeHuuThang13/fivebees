73e403a4a0afd8a85258e53f5007115daef1e45c
nguyen60c <nguyen60c@gmail.com>
2022-07-21 20:21:08 +0700

    features api axios and login components

--- host_application/src/context/Provider.js
+++ host_application/src/context/Provider.js
@@ -2,7 +2,8 @@ import React, {createContext, useReducer} from 'react';
 import auth from './reducers/auth';
 import authInitialState from './initialStates/authInitialState';
 
-export const GlobalContext = createContext();
+export const GlobalContext = createContext({});
+
 const GlobalProvider = ({children}) => {
   const [authState, authDispatch] = useReducer(auth, authInitialState);
 
