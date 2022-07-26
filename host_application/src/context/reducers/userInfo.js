6831ec3c59166078238ed43103f00e5d0db28227
nguyen60c <nguyen60c@gmail.com>
2022-07-26 09:16:49 +0700

    features user profile

---
+++ host_application/src/context/reducers/userInfo.js
@@ -0,0 +1,44 @@
+import {
+  LOGIN_FAILED,
+  LOGIN_LOADING,
+  LOGIN_SUCCESS,
+  LOGOUT_USER,
+} from '../../constants/actionTypes';
+
+const auth = (state, {type, payload}) => {
+  switch (type) {
+    case LOGIN_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: payload,
+        isLoggedIn: true,
+      };
+
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
+
+    case LOGOUT_USER:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        isLoggedIn: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default auth;
